24405d11c110258843db88fe82cd8f7ed00628cb
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-12-27 20:09:03 +0700

    config .env

--- src/services/API/index.js
+++ src/services/API/index.js
@@ -1,3 +1,3 @@
 export const URL_FAKE_DOMAIN = "https://jsonplaceholder.typicode.com/"
   // some api
-export const  URL_DOMAIN_V1 = "https://44.193.93.193/api"
+export const  URL_DOMAIN_V1 = process.env.REACT_APP_API_DOMAIN_V1
